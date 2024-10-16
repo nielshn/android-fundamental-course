932b80e5478ffece91c0f58578dd2597517c6ab7
nielshn <niel.shn08@gmail.com>
2024-10-16 17:08:13 +0700

    learning AppBar android

---
+++ MyAppBar/app/src/main/java/com/dicoding/myappbar/MenuActivity.kt
@@ -0,0 +1,37 @@
+package com.dicoding.myappbar
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.myappbar.databinding.ActivityMenuBinding
+
+class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    searchBar.setText(searchView.text)
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
+    }
+}
