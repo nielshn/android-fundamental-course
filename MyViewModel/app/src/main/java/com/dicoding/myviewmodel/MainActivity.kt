f1f1fb41ec3390f0bd4e18c3214a21a0bb5bd89b
nielshn <niel.shn08@gmail.com>
2024-10-16 17:19:40 +0700

    practicing lifecycle using view model

---
+++ MyViewModel/app/src/main/java/com/dicoding/myviewmodel/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.dicoding.myviewmodel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.MainViewModel
+import com.dicoding.myviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() || height.isEmpty() || length.isEmpty() -> {
+                    binding.tvResult.text = "Please input all values"
+                }
+
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
+    }
+}
